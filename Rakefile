ea1efd6bbaf79702ddd41887ca2af076d941e60f
Henrik <henrik@haf.se>
2012-08-24 15:09:04 +0200

    Test framework commit.
    Adding puppetlabs_spec_helper gem from @puppetlabs for
    extra points towards the module bounty.
    
    Adding corresponding rakefile.
    
    The blog post from the labs about travis integration testing could
    mention travis-ci.org and that it's possible to build well using
    that.

---
+++ Rakefile
@@ -0,0 +1,5 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+require 'rubygems'
+require 'puppetlabs_spec_helper/rake_tasks'
--- spec/classes/riak_spec.rb
+++ spec/classes/riak_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe 'riak', type => :class do
-  let :facts do { :operatingsystem => 'ubuntu' } ; end
+
+  let :facts do 
+    { :operatingsystem => 'ubuntu' }
+  end
+  
   describe 'when using default params' do
-    let :params {}
+    let :params do 
+      {}
+    end
+
     it 'should include the riak class' do
       subject.should contain_class('riak')
     end
+  end
 end
