b639b042106909c4b77ad9b6fb1efd9fa97c8b37
Henrik <henrik@haf.se>
2012-08-25 13:51:14 +0200

    pending specs
    The package specs don't pass because they rely on the httpfile
    type and its provider. However, rspec-puppet doesn't support
    loading custom types as of now.
    
    See https://github.com/rodjek/rspec-puppet/pull/41 and
    the puppet blog post about these three modules: my comment.

--- spec/classes/package_spec.rb
+++ spec/classes/package_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
+require 'puppet'
+
+##mp = RSpec.configuration.module_path
+##Puppet[:modulepath] = Puppet[:modulepath] + ':' + mp if mp
+##libs = Puppet[:modulepath].split(':').collect{ |p| Dir["#{p}/*/lib"].entries }.flatten.join(File::PATH_SEPARATOR)
+##puts "libs: "+libs
+##Puppet[:libdir] = libs
 
 describe 'riak::package', :type => :class do
 
@@ -20,17 +27,19 @@ describe 'riak::package', :type => :class do
     end
 
     it 'should be downloading latest' do
+      pending "waiting for working lib folder for custom types"
       subject.should contain_httpfile('/tmp/custom_riak-1.2.0.deb').with({
         :path => '/tmp/custom_riak-1.2.0.deb',
         :hash => 'abcd'
       })
     end
 
-    it {
+    it 'should have the riak package' do 
+      pending "waiting for working lib folder for custom types"
       should contain_package('custom_riak').with({
         :ensure => 'installed'
       })
-    }
+    end 
     
   end
 end
