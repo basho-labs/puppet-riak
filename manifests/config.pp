d81cebaa7292d9799244db68108a889aee04c3a6
James Martin <jmartin@basho.com>
2012-09-21 00:15:05 -0400

    Made install from URL optional.

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,7 +7,8 @@
 # a "status" parameter, yet.  Working around that for now.
 
 class riak::config( 
-	$absent = false
+	$absent = false,
+        $manage_repos = true
 ) {
 
   $package_repo_type = $::operatingsystem ? {
@@ -26,40 +27,41 @@ class riak::config(
     default => 'present'
   }
 
-
+  if $manage_repos == true {
 	case $package_repo_type {
-	    'apt': {
-	        file { 
-	          'apt-basho':
-	            path    => '/etc/apt/sources.list.d/basho.list',
-	            content => "deb http://apt.basho.com ${lsbdistcodename} main\n",
-	        	ensure  => $manage_apt_repo
-	         }
-	         package { 
-	         	'curl' : ensure => installed 
-	         }
-	         exec {
-	          'add-basho-key':
-	            command  => '/usr/bin/curl http://apt.basho.com/gpg/basho.apt.key | /usr/bin/apt-key add -',
-	            unless   => '/usr/bin/apt-key list | /bin/grep -q "Basho Technologies"',
-	            require  => [ Package['curl']]
-	         }
-	         exec {
-	          'apt-get-update':
-	            command     => '/usr/bin/apt-get update',
-	            subscribe   => File['apt-basho'],
-	            refreshonly => true
-	         }         
+    'apt': {
+        file { 
+          'apt-basho':
+            path    => '/etc/apt/sources.list.d/basho.list',
+            content => "deb http://apt.basho.com ${lsbdistcodename} main\n",
+        	ensure  => $manage_apt_repo
+         }
+         package { 
+         	'curl' : ensure => installed 
+         }
+         exec {
+          'add-basho-key':
+            command  => '/usr/bin/curl http://apt.basho.com/gpg/basho.apt.key | /usr/bin/apt-key add -',
+            unless   => '/usr/bin/apt-key list | /bin/grep -q "Basho Technologies"',
+            require  => [ Package['curl']]
+         }
+         exec {
+          'apt-get-update':
+            command     => '/usr/bin/apt-get update',
+            subscribe   => File['apt-basho'],
+            refreshonly => true
+         }         
 	     }
-	     'yum': {
-	     	yumrepo {
-				'basho-products':
-				  descr 	=> "basho packages for \$releasever-\$basearch",
-				  baseurl 	=> "http://yum.basho.com/el/6/products/\$basearch",
-				  gpgcheck 	=> 1,
-				  enabled 	=> $manage_yum_repo,
-				  gpgkey 	=> "http://yum.basho.com/gpg/RPM-GPG-KEY-basho"				  
-	     	}	     	
+     'yum': {
+       	yumrepo {
+  			'basho-products':
+  			  descr 	=> "basho packages for \$releasever-\$basearch",
+  			  baseurl 	=> "http://yum.basho.com/el/6/products/\$basearch",
+  			  gpgcheck 	=> 1,
+  			  enabled 	=> $manage_yum_repo,
+  			  gpgkey 	=> "http://yum.basho.com/gpg/RPM-GPG-KEY-basho"				  
+       	}	     	
 	     }
     }
+  }
 }     
