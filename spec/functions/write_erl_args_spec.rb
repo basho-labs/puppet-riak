5e78b86bc46c17e4b233b826289a769f3459f583
Henrik <henrik@haf.se>
2013-01-28 10:02:16 +0100

    calling functions with arrays like error msg states

--- spec/functions/write_erl_args_spec.rb
+++ spec/functions/write_erl_args_spec.rb
@@ -3,12 +3,12 @@
 require 'spec_helper'
 
 describe 'write_erl_args', :type => :puppet_function do
-  it { subject.call({
+  it { subject.call([{
     '-name' => 'riak-5',
     '-env'  => {
       :ERL_MAX_PORTS => 4096
     }
-  }).
+  }]).
     should eq(
 "-env ERL_MAX_PORTS 4096
 -name riak-5") }
--- spec/functions/write_erl_config_spec.rb
+++ spec/functions/write_erl_config_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 describe 'write_erl_config', :type => :puppet_function do
-  it { subject.call({ :a => 4 }, :to_s).should eq('[{a, 4}].') }
+  it { subject.call([{ :a => 4 }, :to_s]).should eq('[{a, 4}].') }
   it 'should gen correct erlang' do
-    output = subject.call({
+    output = subject.call([{
       :lager => {
         :handlers => {
           :lager_file_backend   => [
@@ -14,11 +14,11 @@ describe 'write_erl_config', :type => :puppet_function do
           :error_logger_redirect => true
         }
       }
-    }, :to_s)
+    }, :to_s])
     output.should eq('[{lager, [{handlers, [{error_logger_redirect, true}, {lager_file_backend, [{"/var/log/riak/error.log", error, 10485760, "$D0", 5}]}]}]}].')
   end
   it 'should output numbers as numbers' do
-    subject.call({ :a => [['__tuple', '__atom_mykey', 5 ]]}, :to_s).
+    subject.call([{ :a => [['__tuple', '__atom_mykey', 5 ]]}, :to_s]).
       should eq('[{a, [{mykey, 5}]}].')
   end
 end
