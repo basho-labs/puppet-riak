c45db7214cbf6ea572960e919e51be34f5bb4493
Henrik <henrik@haf.se>
2013-04-11 20:36:29 +0200

    working merging

--- lib/puppet/parser/functions/merge_hashes.rb
+++ lib/puppet/parser/functions/merge_hashes.rb
@@ -1,3 +1,5 @@
+require File.join(File.dirname(__FILE__), '/../util/merger')
+
 module Puppet::Parser::Functions
   newfunction(:merge_hashes, :type => :rvalue, :doc => <<-DOC) do |*args|
     Merge two hierarchial hashes together, keeping
@@ -6,7 +8,7 @@ module Puppet::Parser::Functions
     hash, to produce a final hash. Have a look at the source code
     for further comments and documentation.
   DOC
-  
+
     if args[0].is_a? Array
       args = args[0]
     end
@@ -14,29 +16,6 @@ module Puppet::Parser::Functions
     raise(Puppet::ParseError, "merge_hashes/2: Wrong number of arguments " +
           "given (#{args.size} instead of 2.)") unless args.size == 2
   
-    extend args[0], args[1]
-  end
-  
-  private
-  def extend kvs1, kvs2
-    raise ArgumentError unless is_hashish? kvs1
-    raise ArgumentError unless is_hashish? kvs2
-    # if kvs2 didn't have value, return kvs1
-    return kvs1 if kvs2.nil?
-    merged = kvs1.map { |k, v| 
-      # recurse on those keys that have hashish values
-      is_hashish?(v) && is_hashish?(kvs2.fetch(k)) ?
-        # recurse
-        [k, extend(v, kvs2.fetch(k))] :
-        # get the value from kvs2, defaulting to value from kvs1
-        [k, kvs2.fetch(k, v)]
-    # union merge all items of kvs2
-    } | kvs2.reject { |k, v| kvs1.has_key? k }.to_a
-    Hash[ merged ]
-  end
-  
-  # the police is coming
-  def is_hashish? thing
-    [:each, :fetch, :"has_key?", :reject].all? { |m| thing.respond_to? m }
+    ::Puppet::Parser::Util::Merger.new(args[0], args[1]).compute
   end
 end
---
+++ lib/puppet/parser/util/merger.rb
@@ -0,0 +1,35 @@
+module Puppet::Parser::Util
+  class Merger
+    
+    attr_reader :h1, :h2
+
+    def initialize h1, h2
+      @h1, @h2 = h1, h2
+      raise ArgumentError unless is_hashish?(h1)
+      raise ArgumentError unless is_hashish?(h2)
+    end
+    def compute
+      compute_inner h1, h2
+    end
+    def compute_inner kvs1, kvs2
+      # if kvs2 didn't have value, return kvs1
+      return kvs1 if kvs2.nil?
+
+      merged = kvs1.map { |k, v| 
+        # recurse on those keys that have hashish values
+        is_hashish?(v) && is_hashish?(kvs2.fetch(k, nil)) ?
+          # recurse
+          [k, compute_inner(v, kvs2.fetch(k, nil))] :
+          # get the value from kvs2, defaulting to value from kvs1
+          [k, kvs2.fetch(k, v)]
+      # union merge all items of kvs2
+      } | kvs2.reject { |k, v| kvs1.has_key? k }.to_a
+
+      Hash[ merged ]
+    end
+    def is_hashish? thing
+      return false if thing.nil?
+      [:each, :fetch, :"has_key?", :reject].all? { |m| thing.respond_to? m }
+    end
+  end
+end
