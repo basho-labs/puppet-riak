79c9ebfed7d77c34f837f6bbe4fd0aba6388c43b
davidx <davidx@gmail.com>
2014-01-15 21:07:03 -0800

    &&

--- manifests/join.pp
+++ manifests/join.pp
@@ -6,7 +6,7 @@ class riak::join (
 
   exec { "join $host":
     path => '/usr/bin:/usr/sbin:/bin:/sbin',
-    command => "riak ping && riak-admin cluster join riak@$host; riak-admin cluster plan; riak-admin cluster commit",
+    command => "riak ping && riak-admin cluster join riak@$host && riak-admin cluster plan && riak-admin cluster commit",
     onlyif => [ "riak-admin ringready|grep -v $host",
 	        "test $host != $::ipaddress" ]
   }
