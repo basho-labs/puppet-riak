26b6069374b72603c58638bdb8bff23ab3b74232
davidx <davidx@gmail.com>
2014-01-15 21:06:12 -0800

    join

--- manifests/join.pp
+++ manifests/join.pp
@@ -1,13 +1,14 @@
 class riak::join (
 
-  $host    = hiera('join_host', ''),
+  $host = hiera('join_host', ''),
 
 ) inherits riak::params {
 
   exec { "join $host":
-    command => "riak ping && riak-admin cluster join riak@$host"
-    onlyif => "grep -c old_account /etc/passwd",
-
+    path => '/usr/bin:/usr/sbin:/bin:/sbin',
+    command => "riak ping && riak-admin cluster join riak@$host; riak-admin cluster plan; riak-admin cluster commit",
+    onlyif => [ "riak-admin ringready|grep -v $host",
+	        "test $host != $::ipaddress" ]
   }
 
 }
