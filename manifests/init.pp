77f56258cbdb734dace82c364514197a58f1428a
cornellio <peter.cornell@me.com>
2015-05-04 16:08:51 -0700

    add typing of ulimits parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,13 +8,13 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class riak (
-  String[1] $package_name = $::riak::params::package_name,
-  String[1] $service_name = $::riak::params::service_name,
-  Boolean $manage_package = $::riak::params::manage_package,
-  Boolean $manage_repo    = $::riak::params::manage_repo,
-  String[1] $version      = $::riak::params::version,
-  $ulimits_nofile_soft    = $::riak::params::ulimits_nofile_soft,
-  $ulimits_nofile_hard    = $::riak::params::ulimits_nofile_hard,
+  String[1] $package_name       = $::riak::params::package_name,
+  String[1] $service_name       = $::riak::params::service_name,
+  Boolean $manage_package       = $::riak::params::manage_package,
+  Boolean $manage_repo          = $::riak::params::manage_repo,
+  String[1] $version            = $::riak::params::version,
+  Integer $ulimits_nofile_soft  = $::riak::params::ulimits_nofile_soft,
+  Integer $ulimits_nofile_hard  = $::riak::params::ulimits_nofile_hard,
   Hash[String, Variant[String, Boolean, Integer]] $settings = {},
 ) inherits ::riak::params {
   if $manage_repo and $manage_package {
