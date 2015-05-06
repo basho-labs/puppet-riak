937ef9ed4b087e74d4d7dcd757e862ceab343e30
cornellio <peter.cornell@me.com>
2015-05-06 15:22:22 -0700

    Update file content checks for /etc/security/limits.conf (clean)

--- spec/classes/riak_spec.rb
+++ spec/classes/riak_spec.rb
@@ -24,20 +24,15 @@ describe 'riak' do
           it { is_expected.to contain_class('riak::config') }
           it { is_expected.to contain_package('riak').with_ensure('present') }
           it { is_expected.to contain_service('riak') }
-          # it { is_expected.to contain_file('/etc/security/limits.conf').with_content(/riak hard nofile 65536/) }
-          # it { is_expected.to contain_file('/etc/security/limits.conf').with_content(/riak soft nofile 65536/) }
-
           case facts[:osfamily]
-            when 'RedHat'
-              it { is_expected.to contain_class('riak::repository::el') }
-              it { is_expected.to contain_yumrepo('basho_riak') }
-            when 'Debian'
-              it { is_expected.to contain_class('riak::repository::debian') }
-              it { is_expected.to contain_apt__source('riak') }
+          when 'RedHat'
+            it { is_expected.to contain_class('riak::repository::el') }
+            it { is_expected.to contain_yumrepo('basho_riak') }
+          when 'Debian'
+            it { is_expected.to contain_class('riak::repository::debian') }
+            it { is_expected.to contain_apt__source('riak') }
           end
-
         end
-
         context "riak class with repositories disabled" do
           let(:params) {{
             :manage_repo => false
@@ -58,7 +53,6 @@ describe 'riak' do
             it { is_expected.to_not contain_apt__source('riak') }
           end
         end
-
         context "riak class with package set to specific version" do
           let(:params) {{
             :version => '2.0.5'
@@ -66,7 +60,6 @@ describe 'riak' do
           it_behaves_like "class required behavior"
           it { is_expected.to contain_package('riak').with_ensure('2.0.5') }
         end
-
         context "riak class with package install disabled" do
           let(:params) {{
             :manage_package => false
@@ -75,7 +68,6 @@ describe 'riak' do
           it { is_expected.to_not contain_package('riak') }
           it { is_expected.to_not contain_class('riak::install') }
         end
-
         context "riak class with custom package name" do
           let(:params) {{
             :package_name => 'ermongo'
@@ -83,7 +75,6 @@ describe 'riak' do
           it_behaves_like "class required behavior"
           it { is_expected.to contain_package('ermongo') }
         end
-
         context "riak class with custom service name" do
           let(:params) {{
             :service_name => 'ermongo'
@@ -91,7 +82,6 @@ describe 'riak' do
           it_behaves_like "class required behavior"
           it { is_expected.to contain_service('ermongo') }
         end
-
         context "riak class with custom config settings" do
           let(:params) {{
             :settings => {
@@ -103,7 +93,6 @@ describe 'riak' do
           it { is_expected.to contain_file('/etc/riak/riak.conf').with_content(/foo = bar/) }
           it { is_expected.to contain_file('/etc/riak/riak.conf').with_content(/dtrace = on/) }
         end
-
       end
     end
   end
